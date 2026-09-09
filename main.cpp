d8747b1730cc680af9960e135da8ed83481107ae
Victor Schmidt <vicoschmidt@gmail.com>
2026-09-09 00:15:55 -0300

    Att

--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@ template<typename T_container, typename T = typename enable_if<!is_same<T_contai
 void dbg_out() { cerr << endl; }
 template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
 #ifdef LOCAL
-#define debug(...) cerr << "[DEBUG] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#define dbg(...) cerr << "[DEBUG] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 #else
-#define debug(...)
+#define dbg(...)
 #endif
 
 void solve() {
